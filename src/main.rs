43fc6477e60f8b0f117e438a949aa1ed9962fd3a
ihab65 <ihg.bk65@gmail.com>
2023-08-20 17:05:38 -0400

    parsing xml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::{File, self},
+    io, path::Path
+};
+use xml::reader::{
+    XmlEvent::Characters,
+    EventReader
+};
+
+fn read_xml_file<P: AsRef<Path>>(file_path: &P) -> io::Result<String> {
+    let file = File::open(file_path)?;
+    let mut content = String::new();
+
+    for event in EventReader::new(file).into_iter() {
+        if let Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+            
+        }
+    }
+    Ok(content)
+}
+
+fn main() -> io::Result<()> {
+    let dir_path = "docs.gl/gl4";
+    let dir = fs::read_dir(dir_path)?;
+
+    for entry in dir {
+        let entry = entry?;
+        let file_path = entry.path();
+        let xml_content = read_xml_file(&file_path);
+        
+        println!("{file_path:?} => {size}", size = xml_content?.len())
+    }
+
+    Ok(())
 }
