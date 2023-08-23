21bf6473a89ec7a630e9f9270416038c211da5b8
ihab65 <ihg.bk65@gmail.com>
2023-08-23 08:32:26 -0400

    indexing to json and reading data from it

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{ fs::File, io, path::Path, collections::HashMap };
+use std::{ fs::{File, self}, io, path::{Path, PathBuf}, collections::HashMap };
 use xml::reader::{ XmlEvent::Characters, EventReader };
-
+use serde_json::{Result, json};
 
 #[derive(Debug)]
 struct Lexer<'a> {
@@ -75,43 +75,56 @@ fn read_xml_file<P: AsRef<Path>>(file_path: &P) -> io::Result<String> {
     Ok(content)
 }
 
+type TF = HashMap<String, usize>;
+type TFI = HashMap<PathBuf, TF>;
+
 fn main() -> io::Result<()> {
-    let content: Vec<char> = read_xml_file(&"docs.gl/gl4/glWaitSync.xhtml")?
-        .chars()
-        .collect::<Vec<_>>();
+    let index_path = "index.json";
+    let index_file = File::open(index_path)?;
+    println!("Reading {index_path}");
+    let tfi: TFI = serde_json::from_reader(index_file).expect("serde does not fail");
+    println!("{} contains {} files", index_path, tfi.len());
+    
+    Ok(())
+}
 
-    let mut tf = HashMap::<String, usize>::new();
-
-    for token in Lexer::new(&content) {
-        let term = token.iter().map(|x| x.to_ascii_uppercase()).collect::<String>();
-        
-        if let Some(freq) = tf.get_mut(&term) {
-            *freq += 1;
-        } else {
-            tf.insert(term, 1);
-        };
-    }
-
-    let mut stats = tf.iter().collect::<Vec<_>>();
-    stats.sort_by_key(|(_, f)| *f);
-    stats.reverse();
-
-    for (t, f) in stats.iter().take(10) {
-        println!("{t} => {f}")
-    }
-
-    /* 
+fn main2() -> io::Result<()> {
     let dir_path = "docs.gl/gl4";
     let dir = fs::read_dir(dir_path)?;
+    let mut tfi: TFI = TFI::new();
 
     for entry in dir {
-        let entry = entry?;
-        let file_path = entry.path();
-        let xml_content = read_xml_file(&file_path);
+        let file_path = entry?.path();
+
+        println!("Indexing {:?}", &file_path);
+
+        let content: Vec<char> = read_xml_file(&file_path)?
+            .chars()
+            .collect::<Vec<_>>();
+
+        let mut tf: TF = TF::new();
+
+        for token in Lexer::new(&content) {
+            let term = token.iter().map(|x| x.to_ascii_uppercase()).collect::<String>();
         
-        println!("{file_path:?} => {size}", size = xml_content?.len())
+            if let Some(freq) = tf.get_mut(&term) {
+                *freq += 1;
+            } else {
+                tf.insert(term, 1);
+            };
+        }
+
+        let mut stats = tf.iter().collect::<Vec<_>>();
+        stats.sort_by_key(|(_, f)| *f);
+        stats.reverse();
+
+        tfi.insert(file_path, tf);
     }
-    */
+    
+    let index_path = "index.json";
+    println!("Saving to {}", index_path);
+    let index_file = File::create(index_path)?;
+    serde_json::to_writer_pretty(&index_file, &tfi).expect("serde works fine");
 
     Ok(())
 }
